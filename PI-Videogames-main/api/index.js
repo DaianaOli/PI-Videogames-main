c657ad848b5cd9fdc56d7ec12a7d09fb36a25ef8
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-12-24 14:24:48 -0300

    .

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -24,10 +24,15 @@ const dot = require('dotenv');
 dot.config();
 axios.default.baseURL = 'http://localhost:3001';
 var local='';
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+if (process.env.PORT == 3001){
+  local='http://localhost:3001';
+}else {
+  local= "https://pi-henry-videogames.herokuapp.com";
 }
-);
+// Syncing all the models at once.
+console.log(process.env.PORT, local);
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
+  });
+});
