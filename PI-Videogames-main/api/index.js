764e1267f9ac7d876e669ebe1138f30acdf01a1b
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-12-24 14:33:47 -0300

    deploy

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -19,18 +19,9 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const axios = require('axios');
 const dot = require('dotenv');
 dot.config();
-axios.default.baseURL = 'http://localhost:3001';
-var local='';
-if (process.env.PORT == 3001){
-  local='http://localhost:3001';
-}else {
-  local= "https://pi-videogames-main-production-45d2.up.railway.app/";
-}
-// Syncing all the models at once.
-console.log(process.env.PORT, local);
+
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
     console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
