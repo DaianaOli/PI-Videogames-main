cb2f3892627e67e87e1fc00e26aac9143d047cf1
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2022-12-26 11:02:04 -0300

    .

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -19,12 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const dot = require('dotenv');
-dot.config();
-const{ PORT } = process.env;
+const { PORT } = process.env;
+require('dotenv').config();
 
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT ? PORT : 3001, () => {
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
