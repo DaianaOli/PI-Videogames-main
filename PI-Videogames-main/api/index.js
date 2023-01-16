7e33586225315a9653e6e7f8e0a49d8e48cec519
DaianaOli <luzdaianaolivaresolivares@gmail.com>
2023-01-16 10:45:47 -0300

    nueva config

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -20,11 +20,22 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { PORT } = process.env;
-require('dotenv').config();
+const axios = require('axios');
+const dot = require('dotenv');
+dot.config();
+
+axios.default.baseURL = 'http://localhost:3001';
+var local='';
+if (process.env.PORT == 3001){
+  local='http://localhost:3001';
+}else {
+  local= "https://pi-henry-videogames.herokuapp.com";
+}
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+console.log(process.env.PORT, local);
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('\x1b[33m%s\x1b[0m', 'server listening at 3001'); // eslint-disable-line no-console
   });
 });
