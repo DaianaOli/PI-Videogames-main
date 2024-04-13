370b4b5e49e4a3d79b23fa0bc6f102dea66f3cb3
Luz Daiana Olivares <97685217+DaianaOli@users.noreply.github.com>
2024-04-13 16:29:24 -0300

    prueba 02323?

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -21,6 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const PORT = process.env.POSTGRES_PORT || 3000;
 
+
 conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`);
